5fb27c21002f8a21431b3d168ad9ab9c295da7d0
giovane358 <giovane.etec@gmail.com>
2025-10-18 21:34:28 -0300

    Main Spring Boot

---
+++ backend/instituto-busca-longe/src/main/java/br/com/institutobuscalonge/InstitutoBuscaLongeApplication.java
@@ -0,0 +1,13 @@
+package br.com.institutobuscalonge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoBuscaLongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoBuscaLongeApplication.class, args);
+	}
+
+}
---
+++ backend/instituto-busca-longe/src/test/java/br/com/institutobuscalonge/InstitutoBuscaLongeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.institutobuscalonge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoBuscaLongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
